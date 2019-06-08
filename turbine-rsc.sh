c80b9963f36771540c3135a77f0922dd3e505615
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-06-08 23:56:53 +0100

    fill out JAVALIB and SCALALIB in the rsc script

--- turbine-rsc.sh
+++ turbine-rsc.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 # Use semanticdb as input into Turbine
 # Basically, create Java header jars from Scala header jars
 
+mvn package
+
 echo "class C" > C.scala
 cat > D.java <<EOF
 public class D {
@@ -13,7 +15,18 @@ public class D {
 }
 EOF
 
-coursier launch com.twitter:rsc_2.12:0.0.0-768-7357aa0a --main rsc.cli.Main -- -cp $JAVALIB:$SCALALIB -artifacts semanticdb,scalasig -d . C.scala
+# JAVALIB=/path/to/rt.jar
+# SCALALIB=/path/to/scala-library.jar
+
+JAVALIB="${JAVA_HOME}/jre/lib/rt.jar"
+
+function merge_jars() {
+ tr '\n' ':' | sed -re 's#:$##g'
+}
+
+SCALALIB="$(~/coursier fetch org.scala-lang:scala-library:2.12.8 | merge_jars)"
+
+~/coursier launch com.twitter:rsc_2.12:0.0.0-768-7357aa0a --main rsc.cli.Main -- -cp $JAVALIB:$SCALALIB -artifacts semanticdb,scalasig -d . C.scala
 
 java -cp ./target/turbine-0.1-SNAPSHOT-all-deps.jar com.google.turbine.main.Main --classpath $JAVALIB --output out.jar --semanticdbs META-INF/semanticdb/C.scala.semanticdb --sources D.java
 
@@ -23,12 +36,10 @@ cat > E.scala <<EOF
 class E extends C {
   def foo = D.foo()
   def accept(c: C) = ???
-  
+
   accept(foo)
   accept(this)
 }
 EOF
 
-coursier launch org.scala-lang:scala-compiler:2.12.8 --main scala.tools.nsc.Main -- -cp $JAVALIB:$SCALALIB:. E.scala
-
-
+~/coursier launch org.scala-lang:scala-compiler:2.12.8 --main scala.tools.nsc.Main -- -cp $JAVALIB:$SCALALIB:. E.scala
